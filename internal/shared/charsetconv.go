64441c4d843aaece4e18d76b406c8dd700432339
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-10-17 19:51:48 +0200

    Better error message for NewReaderLabel

--- internal/shared/charsetconv.go
+++ internal/shared/charsetconv.go
@@ -10,7 +10,8 @@ import (
 func NewReaderLabel(label string, input io.Reader) (io.Reader, error) {
 	conv, err := charset.NewReaderLabel(label, input)
 	if err != nil {
-		return nil, fmt.Errorf("gofeed/internal/shared: %w", err)
+		return nil, fmt.Errorf(
+			"gofeed: unable create charset converter for %q: %w", label, err)
 	}
 	return conv, nil
 }
