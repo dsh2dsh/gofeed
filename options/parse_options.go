a3e2a2b4e3cd71aad437d75411675928cb159c2b
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-10-17 16:07:52 +0200

    Delete unused `RequestOptions`

--- options/parse_options.go
+++ options/parse_options.go
@@ -1,10 +1,5 @@
 package options
 
-import (
-	"net/http"
-	"time"
-)
-
 // ParseOptions configures how feeds are parsed
 type ParseOptions struct {
 	// Keep reference to the original format-specific feed
@@ -22,28 +17,6 @@ type StrictnessOptions struct {
 	AllowInvalidDates    bool
 	AllowMissingRequired bool
 	AllowUnescapedMarkup bool
-}
-
-// RequestOptions configures HTTP requests for ParseURL
-type RequestOptions struct {
-	UserAgent       string
-	Timeout         time.Duration
-	IfNoneMatch     string    // ETag for conditional requests
-	IfModifiedSince time.Time // For conditional requests
-	Client          *http.Client
-	AuthConfig      *Auth
-}
-
-// Auth is a structure allowing to use the BasicAuth during the HTTP request. It
-// must be instantiated with your new Parser.
-type Auth struct {
-	Username string
-	Password string
-}
-
-// Empty return true if this Auth has not Username and Password.
-func (self *Auth) Empty() bool {
-	return self.Username == "" && self.Password == ""
 }
 
 // DefaultParseOptions returns sensible defaults
