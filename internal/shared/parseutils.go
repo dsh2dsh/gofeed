60157d29616b1296bdd848d2fce54fa29c71a2d1
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-11-11 22:52:55 +0100

    ParseNameAddress returns input string if it can't parse it

--- internal/shared/parseutils.go
+++ internal/shared/parseutils.go
@@ -11,28 +11,27 @@ var (
 	emailOnlyRgx = regexp.MustCompile(`^([^@()]+@[^@()]+)$`)
 )
 
-// ParseNameAddress parses name/email strings commonly
-// found in RSS feeds of the format "Example Name (example@site.com)"
-// and other variations of this format.
-func ParseNameAddress(nameAddressText string) (name, address string) {
-	if nameAddressText == "" {
+// ParseNameAddress parses name/email strings commonly found in RSS feeds of the
+// format "Example Name (example@site.com)" and other variations of this format.
+func ParseNameAddress(s string) (name, address string) {
+	if s == "" {
 		return "", ""
 	}
 
-	if m := emailNameRgx.FindStringSubmatch(nameAddressText); m != nil {
+	if m := emailNameRgx.FindStringSubmatch(s); m != nil {
 		return m[2], m[1]
 	}
 
-	if m := nameEmailRgx.FindStringSubmatch(nameAddressText); m != nil {
+	if m := nameEmailRgx.FindStringSubmatch(s); m != nil {
 		return m[1], m[2]
 	}
 
-	if m := nameOnlyRgx.FindStringSubmatch(nameAddressText); m != nil {
+	if m := nameOnlyRgx.FindStringSubmatch(s); m != nil {
 		return m[1], ""
 	}
 
-	if m := emailOnlyRgx.FindStringSubmatch(nameAddressText); m != nil {
+	if m := emailOnlyRgx.FindStringSubmatch(s); m != nil {
 		return "", m[1]
 	}
-	return "", ""
+	return s, ""
 }
