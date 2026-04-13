d5e9e4652df11e1a85f7061e8c85d49701313fc6
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2026-04-13 22:05:27 +0200

    Add more tests for `date` package from miniflux
    https://github.com/miniflux/v2/blob/main/internal/reader/date/parser_test.go

--- internal/date/parser_test.go
+++ internal/date/parser_test.go
@@ -94,3 +94,67 @@ func TestParseDateGMTUnchanged(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, time.Date(2025, 4, 21, 6, 0, 0, 0, time.UTC), parsed.UTC())
 }
+
+func FuzzParse(f *testing.F) {
+	f.Add("2017-12-22T22:09:49+00:00")
+	f.Add("Fri, 31 Mar 2023 20:19:00 America/Los_Angeles")
+	f.Fuzz(func(t *testing.T, date string) {
+		Parse(date)
+	})
+}
+
+func TestParseEmptyDate(t *testing.T) {
+	_, err := Parse("  ")
+	require.Error(t, err)
+}
+
+func TestParseInvalidDate(t *testing.T) {
+	_, err := Parse("invalid")
+	require.Error(t, err)
+}
+
+func TestParseAtomDate(t *testing.T) {
+	date, err := Parse("2017-12-22T22:09:49+00:00")
+	require.NoError(t, err)
+	assert.Equal(t, int64(1513980589), date.Unix())
+
+	_, offset := date.Zone()
+	assert.Zero(t, offset)
+}
+
+func TestParseRSSDateGMT(t *testing.T) {
+	date, err := Parse("Tue, 03 Jun 2003 09:39:21 GMT")
+	require.NoError(t, err)
+	assert.Equal(t, time.Unix(1054633161, 0), date)
+}
+
+func TestParseRSSDatePST(t *testing.T) {
+	date, err := Parse("Wed, 26 Dec 2018 10:00:54 PST")
+	require.NoError(t, err)
+	assert.Equal(t, int64(1545847254), date.Unix())
+	assert.Equal(t, "PST", date.Location().String())
+
+	name, offset := date.Zone()
+	assert.Equal(t, "PST", name)
+	assert.Equal(t, -28800, offset)
+}
+
+func TestParseRSSDateEST(t *testing.T) {
+	date, err := Parse("Wed, 10 Feb 2021 22:46:00 EST")
+	require.NoError(t, err)
+	assert.Equal(t, int64(1613015160), date.Unix())
+	assert.Equal(t, "EST", date.Location().String())
+
+	name, offset := date.Zone()
+	assert.Equal(t, "EST", name)
+	assert.Equal(t, -18000, offset)
+}
+
+func TestParseRSSDateOffset(t *testing.T) {
+	date, err := Parse("Sun, 28 Oct 2018 13:48:00 +0100")
+	require.NoError(t, err)
+	assert.Equal(t, int64(1540730880), date.Unix())
+
+	_, offset := date.Zone()
+	assert.Equal(t, 3600, offset)
+}
