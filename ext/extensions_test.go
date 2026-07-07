073c6f6d306312b0ce46a6d573be0f36c2234a4f
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2026-07-07 20:13:52 +0200

    Move media extension test files upper

--- ext/extensions_test.go
+++ ext/extensions_test.go
@@ -10,38 +10,48 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/dsh2dsh/gofeed/v2"
 )
 
-func TestMedia_Extensions(t *testing.T) {
-	files, _ := filepath.Glob("testdata/media/*.xml")
+func TestParse(t *testing.T) {
+	files, _ := filepath.Glob("testdata/*.xml")
 	for _, f := range files {
 		base := filepath.Base(f)
 		name := strings.TrimSuffix(base, filepath.Ext(base))
 
-		fmt.Printf("Testing %s... ", name)
+		t.Run(name, func(t *testing.T) {
+			t.Logf("Testing %s ... ", base)
 
-		// Get actual source feed
-		ff := fmt.Sprintf("testdata/media/%s.xml", name)
-		f, _ := os.ReadFile(ff)
+			// Get json encoded expected feed result
+			e, err := os.ReadFile(fmt.Sprintf("testdata/%s.json", name))
+			require.NoError(t, err)
 
-		// Parse actual feed
-		fp := gofeed.NewParser()
-		actual, _ := fp.Parse(bytes.NewReader(f))
+			// Unmarshal expected feed
+			var expected struct {
+				gofeed.Feed
 
-		// Get json encoded expected feed result
-		ef := fmt.Sprintf("testdata/media/%s.json", name)
-		e, _ := os.ReadFile(ef)
+				ErrorContains string `json:"errorContains"`
+			}
+			require.NoError(t, json.Unmarshal(e, &expected))
 
-		// Unmarshal expected feed
-		expected := &gofeed.Feed{}
-		json.Unmarshal(e, &expected)
+			// Get actual source feed
+			f, err := os.ReadFile(fmt.Sprintf("testdata/%s.xml", name))
+			require.NoError(t, err)
 
-		if assert.Equal(t, expected, actual, "Feed file %s.xml did not match expected output %s.json", name, name) {
-			fmt.Printf("OK\n")
-		} else {
-			fmt.Printf("Failed\n")
-		}
+			// Parse actual feed
+			feed, err := gofeed.NewParser().Parse(bytes.NewReader(f))
+
+			if expected.ErrorContains != "" {
+				t.Log(err)
+				require.ErrorContains(t, err, expected.ErrorContains)
+				return
+			}
+
+			require.NoError(t, err)
+			assert.Equal(t, &expected.Feed, feed,
+				"Feed file %s.xml did not match expected output %s.json", name, name)
+		})
 	}
 }
